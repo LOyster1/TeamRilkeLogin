0f003b0a077592a395db31ead0f9b8ff13e29a06
ericpak <pak.eric@gmail.com>
2016-10-08 16:37:46 -0800

    username OR email login
    - Added the ability to login with either username or email

--- script/check-login.php
+++ script/check-login.php
@@ -5,13 +5,14 @@ require 'database.php';
 	if(!empty($_POST['username_email']) && !empty($_POST['password'])):
 		
 		// Retrives the record of the entered username from the user table
-		$records = $conn->prepare('SELECT * FROM user WHERE username = :username');
-		$records->bindParam(':username', $_POST['username_email']);
-		$records->execute();
+		$records = $conn->prepare('SELECT * FROM user WHERE username = :username OR email = :email LIMIT 1');
+		$records->execute(array(':username'=>$_POST['username_email'], ':email'=>$_POST['username_email']));
 		$results = $records->fetch(PDO::FETCH_ASSOC);
 		
+		print_r($results);
+		echo "hi";
 		// Checks to see if the password matches
-		if(count($results) > 0 && password_verify($_POST['password'], $results['password']))
+		if(password_verify($_POST['password'], $results['password']))
 		{
 			// Sets the session's 'user_id' to the user's 'id'
 			$_SESSION['user_id'] = $results['id'];
